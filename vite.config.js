4cd729148940034b8f8eb25a42f1d83ab901ff28
volcanorc <sirjone100@gmail.com>
2025-11-23 19:16:28 +0800

    fix error v2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio/", // GitHub Pages subfolder
+  base: "/portfolio1/", // GitHub Pages subfolder
 });
